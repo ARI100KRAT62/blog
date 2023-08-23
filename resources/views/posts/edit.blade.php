333fe8494b99f1a15c08c7979338120ebf7c1270
ARI100KRAT62 <niss162@mail.ru>
2023-08-23 20:56:21 +0300

    кнопка удаления поста

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -29,8 +29,11 @@
     </label>
 
     <button type="submit">Сохранить</button>
-</form>
-
+    <form action="{{ route('posts.destroy', [$post]) }}" method="post">
+        @csrf
+        @method('delete')
+        <button type="submit" class="btn">Удалить</button>
+    </form>
 <script>
     ClassicEditor
         .create( document.querySelector( '#content' ) )
